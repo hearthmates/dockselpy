ab8c7496499472bc4536d5fde8da9886a35fedef
cesar <cesar@hearthapp.io>
2023-06-15 00:57:20 -0400

    Jun 2023 Stability fixes
    - force actions run

--- example.py
+++ example.py
@@ -19,7 +19,6 @@ def chrome_example():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('-headless')
     chrome_options.add_argument('--no-sandbox')
-
     chrome_options.add_experimental_option('prefs', {
         'download.default_directory': os.getcwd(),
         'download.prompt_for_download': False,
